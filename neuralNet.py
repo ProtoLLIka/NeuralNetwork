3a99c5cd9484cab93646c999f119b28d984266ed
ProtoLLIka <49634186+ProtoLLIka@users.noreply.github.com>
2020-07-06 20:01:48 +0400

    update

--- neuralNet.py
+++ neuralNet.py
@@ -6,3 +6,10 @@ class NeuralNetwork:
     def __init__(self):
         weights = np.array([0, 1])
         bias = 0
+        # Скрытый слой из 6 нейронов
+        self.hidden1 = Neuron(weights, bias)
+        self.hidden2 = Neuron(weights, bias)
+        self.hidden3 = Neuron(weights, bias)
+        self.hidden4 = Neuron(weights, bias)
+        self.hidden5 = Neuron(weights, bias)
+        self.hidden6 = Neuron(weights, bias)
